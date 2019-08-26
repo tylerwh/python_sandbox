d881c1665cbc135dc3dc0b55ccc640fb9992cac1
Tyler Hochstetler <tylerw.hochstetler@gmail.com>
2019-08-26 07:59:10 -0500

    Tried removing #comments to fix error

--- python_sandbox_starter/strings.py
+++ python_sandbox_starter/strings.py
@@ -1,10 +1,2 @@
-# Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
-
-
-# String Formatting
-
-# String Methods
 x = "hello world"
-
-# Capitalize
 print(x.capitalize())
